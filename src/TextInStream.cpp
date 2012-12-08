47d8dd67b7b85a9a49c00a219ef4ef59aafc73f7
airtrack <superking.airtrack@gmail.com>
2012-12-08 23:31:18 +0800

    Replace tabs by spaces.

--- src/TextInStream.cpp
+++ src/TextInStream.cpp
@@ -3,17 +3,17 @@
 namespace io {
 namespace text {
 
-	InStream::InStream(const std::string& path)
-		: stream_(nullptr)
-	{
-		stream_ = fopen(path.c_str(), "rb");
-	}
+    InStream::InStream(const std::string& path)
+        : stream_(nullptr)
+    {
+        stream_ = fopen(path.c_str(), "rb");
+    }
 
-	InStream::~InStream()
-	{
-		if (stream_)
-			fclose(stream_);
-	}
+    InStream::~InStream()
+    {
+        if (stream_)
+            fclose(stream_);
+    }
 
 } // namespace text
 } // namespace io
