475fe3dd43ef504d0d5188ef7d3b00067f81d73d
airtrack <superking.airtrack@gmail.com>
2013-02-09 16:01:24 +0800

    Implement table type.

--- src/Table.cpp
+++ src/Table.cpp
@@ -1,9 +1,166 @@
 #include "Table.h"
+#include <math.h>
+
+namespace
+{
+    inline bool IsInt(double d)
+    {
+        return floor(d) == d;
+    }
+} // namespace
 
 namespace luna
 {
     Table::Table()
-        : array_(nullptr), hash_(nullptr)
     {
+    }
+
+    bool Table::SetArrayValue(std::size_t index, const Value &value)
+    {
+        if (index < 1)
+            return false;
+
+        std::size_t array_size = ArraySize();
+        if (index > array_size + 1)
+            return false;
+
+        if (index == array_size + 1)
+        {
+            AppendToArray(value);
+
+            // move all continuous key from hash to array
+            Value key;
+            key.num_ = ++index;
+            key.type_ = ValueT_Number;
+            while (MoveHashToArray(key))
+                key.num_ = ++index;
+        }
+        else
+        {
+            (*array_)[index - 1] = value;
+        }
+
+        return true;
+    }
+
+    void Table::SetValue(const Value &key, const Value &value)
+    {
+        // Try array part
+        if (key.type_ == ValueT_Number && IsInt(key.num_))
+        {
+            if (SetArrayValue(static_cast<std::size_t>(key.num_), value))
+                return ;
+        }
+
+        // Hash part
+        if (!hash_)
+            hash_.reset(new Hash);
+
+        auto it = hash_->find(key);
+        if (it != hash_->end())
+            it->second = value;
+        else
+            hash_->insert(std::make_pair(key, value));
+    }
+
+    Value Table::GetValue(const Value &key) const
+    {
+        // Get from array first
+        if (key.type_ == ValueT_Number && IsInt(key.num_))
+        {
+            std::size_t index = static_cast<std::size_t>(key.num_);
+            if (index >= 1 && index <= ArraySize())
+                return (*array_)[index - 1];
+        }
+
+        // Get from hash table
+        if (hash_)
+        {
+            auto it = hash_->find(key);
+            if (it != hash_->end())
+                return it->second;
+        }
+
+        // key not exist
+        return Value();
+    }
+
+    bool Table::FirstKeyValue(Value &key, Value &value)
+    {
+        // array part
+        if (ArraySize() > 0)
+        {
+            key.num_ = 1;           // first element index
+            key.type_ = ValueT_Number;
+            value = (*array_)[0];
+            return true;
+        }
+
+        // hash part
+        if (hash_ && !hash_->empty())
+        {
+            auto first = hash_->begin();
+            key = first->first;
+            value = first->second;
+            return true;
+        }
+
+        return false;
+    }
+
+    bool Table::NextKeyValue(const Value &key, Value &next_key, Value &next_value)
+    {
+        // array part
+        if (key.type_ == ValueT_Number && IsInt(key.num_))
+        {
+            std::size_t index = static_cast<std::size_t>(key.num_) + 1;
+            if (index >= 1 && index <= ArraySize())
+            {
+                next_key.num_ = index;
+                next_key.type_ = ValueT_Number;
+                next_value = (*array_)[index - 1];
+                return true;
+            }
+        }
+
+        // hash part
+        if (hash_)
+        {
+            auto it = hash_->find(key);
+            if (it != hash_->end() && ++it != hash_->end())
+            {
+                next_key = it->first;
+                next_value = it->second;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    std::size_t Table::ArraySize() const
+    {
+        return array_ ? array_->size() : 0;
+    }
+
+    void Table::AppendToArray(const Value &value)
+    {
+        if (!array_)
+            array_.reset(new Array);
+        array_->push_back(value);
+    }
+
+    bool Table::MoveHashToArray(const Value &key)
+    {
+        if (!hash_)
+            return false;
+
+        auto it = hash_->find(key);
+        if (it == hash_->end())
+            return false;
+
+        AppendToArray(it->second);
+        hash_->erase(it);
+        return true;
     }
 } // namespace luna
--- src/Table.h
+++ src/Table.h
@@ -3,22 +3,55 @@
 
 #include "Value.h"
 #include "GCObject.h"
+#include <memory>
 #include <vector>
 #include <unordered_map>
 
 namespace luna
 {
+    // Table has array part and hash table part.
     class Table : public GCObject
     {
     public:
         Table();
 
+        // Set array value by index, return true if success.
+        // 'index' start from 1.
+        bool SetArrayValue(std::size_t index, const Value &value);
+
+        // Add key-value into table.
+        // If key is number and key fit with array, then insert into array,
+        // otherwise insert into hash table.
+        void SetValue(const Value &key, const Value &value);
+
+        // Get Value of key from array first,
+        // if key is number, then get the value from array when key number
+        // is fit with array as index, otherwise try search in hash table.
+        // Return value is 'nil' if 'key' is not existed.
+        Value GetValue(const Value &key) const;
+
+        // Get first key-value pair of table, return true if table is not empty.
+        bool FirstKeyValue(Value &key, Value &value);
+
+        // Get the next key-value pair by current 'key', return false if there
+        // is no key-value pair any more.
+        bool NextKeyValue(const Value &key, Value &next_key, Value &next_value);
+
     private:
         typedef std::vector<Value> Array;
         typedef std::unordered_map<Value, Value> Hash;
 
-        Array *array_;              // array part of table
-        Hash *hash_;                // hash table part of table
+        std::size_t ArraySize() const;
+
+        // Append value to array.
+        void AppendToArray(const Value &value);
+
+        // Move hash table key-value pair to array which key is number and key
+        // fit with array, return true if move success.
+        bool MoveHashToArray(const Value &key);
+
+        std::unique_ptr<Array> array_;              // array part of table
+        std::unique_ptr<Hash> hash_;                // hash table part of table
     };
 } // namespace luna
 
