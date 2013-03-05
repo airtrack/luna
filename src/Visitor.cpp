e431f85ede5b69186547f4299f26dab0e3540b78
airtrack <superking.airtrack@gmail.com>
2013-03-06 00:27:06 +0800

    Add ScopeNameLevel and ScopeNameManager.

--- src/Visitor.cpp
+++ src/Visitor.cpp
@@ -1,9 +1,113 @@
 #include "Visitor.h"
 #include "State.h"
 #include "Function.h"
+#include <vector>
 
 namespace luna
 {
+    class ScopeNameLevel;
+    class ScopeNameManager;
+
+    class ScopeNameLevel
+    {
+    public:
+        explicit ScopeNameLevel(ScopeNameManager &manager);
+        ~ScopeNameLevel();
+
+        // Get current scope level name by current name level index
+        String * GetScopeName(std::size_t index) const;
+
+        // Add name to scope
+        void AddScopeName(String *name);
+
+    private:
+        // level owner manager
+        ScopeNameManager *manager_;
+
+        // previous scope
+        ScopeNameLevel *previous_;
+
+        // start index in manager_
+        std::size_t start_;
+    };
+
+    class ScopeNameManager
+    {
+        friend class ScopeNameLevel;
+    public:
+        ScopeNameManager() : current_level_(nullptr) { }
+
+        ScopeNameManager(const ScopeNameManager&) = delete;
+        void operator = (const ScopeNameManager&) = delete;
+
+    private:
+        // Set and get current scope level
+        ScopeNameLevel * GetCurrentLevel() const;
+        void SetCurrentLevel(ScopeNameLevel *level);
+
+        // Get name by index, ignore scope name level
+        String * GetName(std::size_t index) const;
+
+        // Get name count
+        std::size_t GetNameCount() const;
+
+        // Add name
+        void AddName(String *name);
+
+        std::vector<String *> name_list_;
+        ScopeNameLevel *current_level_;
+    };
+
+    ScopeNameLevel::ScopeNameLevel(ScopeNameManager &manager)
+        : manager_(&manager), previous_(nullptr), start_(0)
+    {
+        previous_ = manager_->GetCurrentLevel();
+        start_ = manager_->GetNameCount();
+        manager_->SetCurrentLevel(this);
+    }
+
+    ScopeNameLevel::~ScopeNameLevel()
+    {
+        manager_->SetCurrentLevel(previous_);
+    }
+
+    String * ScopeNameLevel::GetScopeName(std::size_t index) const
+    {
+        return manager_->GetName(start_ + index);
+    }
+
+    void ScopeNameLevel::AddScopeName(String *name)
+    {
+        manager_->AddName(name);
+    }
+
+    ScopeNameLevel * ScopeNameManager::GetCurrentLevel() const
+    {
+        return current_level_;
+    }
+
+    void ScopeNameManager::SetCurrentLevel(ScopeNameLevel *level)
+    {
+        current_level_ = level;
+    }
+
+    String * ScopeNameManager::GetName(std::size_t index) const
+    {
+        if (index < name_list_.size())
+            return name_list_[index];
+        return nullptr;
+    }
+
+    std::size_t ScopeNameManager::GetNameCount() const
+    {
+        return name_list_.size();
+    }
+
+    void ScopeNameManager::AddName(String *name)
+    {
+        name_list_.push_back(name);
+    }
+
     class CodeGenerateVisitor : public Visitor
     {
     public:
