474f8e8bd4b3ce0708bfdf70d38f1464d052b857
airtrack <superking.airtrack@gmail.com>
2013-11-03 23:15:10 +0800

    GC class.

--- src/GC.cpp
+++ src/GC.cpp
@@ -5,4 +5,9 @@ namespace luna
     GCObject::~GCObject()
     {
     }
+
+    GC::GC()
+        : gen0_(nullptr), gen1_(nullptr), gen2_(nullptr)
+    {
+    }
 } // namespace luna
--- src/GC.h
+++ src/GC.h
@@ -12,6 +12,7 @@ namespace luna
 
     class GCObject
     {
+        friend class GC;
     public:
         virtual ~GCObject() = 0;
 
@@ -22,6 +23,20 @@ namespace luna
         unsigned int generation_ : 2;
         // GC flag
         unsigned int gc_ : 2;
+    };
+
+    class GC
+    {
+    public:
+        GC();
+
+    private:
+        // Youngest generation object list
+        GCObject *gen0_;
+        // Mesozoic generation object list
+        GCObject *gen1_;
+        // Oldest generation object list
+        GCObject *gen2_;
     };
 } // namespace luna
 
