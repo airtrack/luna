56cf8efbb387f5391286bb82f5023724dd9899fd
airtrack <superking.airtrack@gmail.com>
2013-11-03 22:55:07 +0800

    Add GC object common fields.

--- src/GCObject.h
+++ src/GCObject.h
@@ -3,10 +3,25 @@
 
 namespace luna
 {
+    enum GCGeneration
+    {
+        GCGen0,         // Youngest generation
+        GCGen1,         // Mesozoic generation
+        GCGen2,         // Oldest generation
+    };
+
     class GCObject
     {
     public:
         virtual ~GCObject() = 0;
+
+    private:
+        // Pointing next GCObject in current generation
+        GCObject *next_;
+        // Generation flag
+        unsigned int generation_ : 2;
+        // GC flag
+        unsigned int gc_ : 2;
     };
 } // namespace luna
 
