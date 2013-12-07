77b7d1b152b28048f5734ba92755813b7be230b6
airtrack <superking.airtrack@gmail.com>
2013-12-07 23:53:08 +0800

    Some semantic unittests.

--- test/TestCommon.h
+++ test/TestCommon.h
@@ -7,7 +7,9 @@
 #include "String.h"
 #include "TextInStream.h"
 #include "Exception.h"
+#include "Visitor.h"
 #include <functional>
+#include <type_traits>
 
 class ParserWrapper
 {
@@ -35,6 +37,335 @@ private:
     luna::String name_;
     luna::Lexer lexer_;
     luna::Parser parser_;
+};
+
+#define MATCH_AST_TYPE(ast, not_match_stmt)                             \
+    if (!the_ast_node_)                                                 \
+    {                                                                   \
+        auto f = [&]() not_match_stmt;                                  \
+        SetResult(typename std::conditional<                            \
+            std::is_same<std::remove_reference<decltype(*ast)>::type,   \
+                         ASTType>::value,                               \
+            std::true_type, std::false_type>::type(), ast, f);          \
+    }
+
+template<typename ASTType, typename FinderType>
+class ASTFinder : public luna::Visitor
+{
+public:
+    explicit ASTFinder(const FinderType &finder)
+        : the_ast_node_(nullptr), finder_(finder) { }
+
+    ASTType * GetResult() const
+    {
+        return the_ast_node_;
+    }
+
+    virtual void Visit(luna::Chunk *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            ast->block_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::Block *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            for (auto &stmt : ast->statements_)
+                stmt->Accept(this, nullptr);
+            if (ast->return_stmt_)
+                ast->return_stmt_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::ReturnStatement *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            if (ast->exp_list_)
+                ast->exp_list_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::BreakStatement *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {})
+    }
+
+    virtual void Visit(luna::DoStatement *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            ast->block_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::WhileStatement *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            ast->exp_->Accept(this, nullptr);
+            ast->block_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::RepeatStatement *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            ast->block_->Accept(this, nullptr);
+            ast->exp_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::IfStatement *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            ast->exp_->Accept(this, nullptr);
+            ast->true_branch_->Accept(this, nullptr);
+            if (ast->false_branch_)
+                ast->false_branch_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::ElseIfStatement *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            ast->exp_->Accept(this, nullptr);
+            ast->true_branch_->Accept(this, nullptr);
+            if (ast->false_branch_)
+                ast->false_branch_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::ElseStatement *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            ast->block_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::NumericForStatement *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            ast->exp1_->Accept(this, nullptr);
+            ast->exp2_->Accept(this, nullptr);
+            if (ast->exp3_)
+                ast->exp3_->Accept(this, nullptr);
+            ast->block_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::GenericForStatement *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            ast->name_list_->Accept(this, nullptr);
+            ast->exp_list_->Accept(this, nullptr);
+            ast->block_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::FunctionStatement *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            ast->func_name_->Accept(this, nullptr);
+            ast->func_body_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::FunctionName *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {})
+    }
+
+    virtual void Visit(luna::LocalFunctionStatement *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            ast->func_body_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::LocalNameListStatement *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            ast->name_list_->Accept(this, nullptr);
+            if (ast->exp_list_)
+                ast->exp_list_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::AssignmentStatement *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            ast->var_list_->Accept(this, nullptr);
+            ast->exp_list_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::VarList *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            for (auto &var : ast->var_list_)
+                var->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::Terminator *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {})
+    }
+
+    virtual void Visit(luna::BinaryExpression *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            ast->left_->Accept(this, nullptr);
+            ast->right_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::UnaryExpression *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            ast->exp_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::FunctionBody *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            if (ast->param_list_)
+                ast->param_list_->Accept(this, nullptr);
+            ast->block_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::ParamList *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            if (ast->name_list_)
+                ast->name_list_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::NameList *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {})
+    }
+
+    virtual void Visit(luna::TableDefine *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            for (auto &field : ast->fields_)
+                field->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::TableIndexField *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            ast->index_->Accept(this, nullptr);
+            ast->value_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::TableNameField *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            ast->value_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::TableArrayField *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            ast->value_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::IndexAccessor *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            ast->table_->Accept(this, nullptr);
+            ast->index_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::MemberAccessor *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            ast->table_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::NormalFuncCall *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            ast->caller_->Accept(this, nullptr);
+            ast->args_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::MemberFuncCall *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            ast->caller_->Accept(this, nullptr);
+            ast->args_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::FuncCallArgs *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            ast->arg_->Accept(this, nullptr);
+        })
+    }
+
+    virtual void Visit(luna::ExpressionList *ast, void *)
+    {
+        MATCH_AST_TYPE(ast, {
+            for (auto &exp : ast->exp_list_)
+                exp->Accept(this, nullptr);
+        })
+    }
+
+private:
+    template<typename Type, typename Func>
+    void SetResult(std::true_type, Type *t, const Func &op)
+    {
+        if (finder_(t))
+            the_ast_node_ = t;
+        else
+            op();
+    }
+
+    template<typename Type, typename Func>
+    void SetResult(std::false_type, Type *t, const Func &op)
+    {
+        op();
+    }
+
+    ASTType *the_ast_node_;
+    FinderType finder_;
+};
+
+template<typename ASTType, typename FinderType>
+ASTType * ASTFind(const std::unique_ptr<luna::SyntaxTree> &root,
+                  const FinderType &finder)
+{
+    ASTFinder<ASTType, FinderType> ast_finder(finder);
+    root->Accept(&ast_finder, nullptr);
+    return ast_finder.GetResult();
+}
+
+struct NameFinder
+{
+    NameFinder(const std::string &name) : name_(name) { }
+
+    bool operator () (const luna::Terminator *term) const
+    {
+        if (term->token_.token_ == luna::Token_Id)
+            return term->token_.str_->GetStdString() == name_;
+        else
+            return false;
+    }
+
+    std::string name_;
 };
 
 #endif // TEST_COMMON_H
--- test/TestSemantic.cpp
+++ test/TestSemantic.cpp
@@ -15,5 +15,124 @@ namespace
 
 TEST_CASE(semantic1)
 {
-    auto ast = Semantic("a = b");
+    auto ast = Semantic("a, b = c, d");
+    auto a = ASTFind<luna::Terminator>(ast, NameFinder("a"));
+    auto b = ASTFind<luna::Terminator>(ast, NameFinder("b"));
+    auto c = ASTFind<luna::Terminator>(ast, NameFinder("c"));
+    auto d = ASTFind<luna::Terminator>(ast, NameFinder("d"));
+    EXPECT_TRUE(a->semantic_ == luna::SemanticOp_Write);
+    EXPECT_TRUE(b->semantic_ == luna::SemanticOp_Write);
+    EXPECT_TRUE(c->semantic_ == luna::SemanticOp_Read);
+    EXPECT_TRUE(d->semantic_ == luna::SemanticOp_Read);
+}
+
+TEST_CASE(semantic2)
+{
+    auto ast = Semantic("f(a, b)");
+    auto f = ASTFind<luna::Terminator>(ast, NameFinder("f"));
+    auto a = ASTFind<luna::Terminator>(ast, NameFinder("a"));
+    auto b = ASTFind<luna::Terminator>(ast, NameFinder("b"));
+    EXPECT_TRUE(f->semantic_ == luna::SemanticOp_Read);
+    EXPECT_TRUE(a->semantic_ == luna::SemanticOp_Read);
+    EXPECT_TRUE(b->semantic_ == luna::SemanticOp_Read);
+}
+
+TEST_CASE(semantic3)
+{
+    auto ast = Semantic("m:f(a, b)");
+    auto m = ASTFind<luna::Terminator>(ast, NameFinder("m"));
+    auto a = ASTFind<luna::Terminator>(ast, NameFinder("a"));
+    auto b = ASTFind<luna::Terminator>(ast, NameFinder("b"));
+    EXPECT_TRUE(m->semantic_ == luna::SemanticOp_Read);
+    EXPECT_TRUE(a->semantic_ == luna::SemanticOp_Read);
+    EXPECT_TRUE(b->semantic_ == luna::SemanticOp_Read);
+}
+
+TEST_CASE(semantic4)
+{
+    auto ast = Semantic("t.m.n = a");
+    auto t_m_n = ASTFind<luna::MemberAccessor>(ast, [](luna::MemberAccessor *ma) {
+        return ma->member_.str_->GetStdString() == "n";
+    });
+    auto t_m = ASTFind<luna::MemberAccessor>(ast, [](luna::MemberAccessor *ma) {
+        return ma->member_.str_->GetStdString() == "m";
+    });
+    auto t = ASTFind<luna::Terminator>(ast, NameFinder("t"));
+    auto a = ASTFind<luna::Terminator>(ast, NameFinder("a"));
+    EXPECT_TRUE(t_m_n->semantic_ == luna::SemanticOp_Write);
+    EXPECT_TRUE(t_m->semantic_ == luna::SemanticOp_Read);
+    EXPECT_TRUE(t->semantic_ == luna::SemanticOp_Read);
+    EXPECT_TRUE(a->semantic_ == luna::SemanticOp_Read);
+}
+
+TEST_CASE(semantic5)
+{
+    auto ast = Semantic("t[i][j] = a");
+    auto t_i_j = ASTFind<luna::IndexAccessor>(ast, [](luna::IndexAccessor *) {
+        return true;
+    });
+    auto t_i = ASTFind<luna::IndexAccessor>(t_i_j->table_, [](luna::IndexAccessor *) {
+        return true;
+    });
+    auto t = ASTFind<luna::Terminator>(ast, NameFinder("t"));
+    auto i = ASTFind<luna::Terminator>(ast, NameFinder("i"));
+    auto j = ASTFind<luna::Terminator>(ast, NameFinder("j"));
+    auto a = ASTFind<luna::Terminator>(ast, NameFinder("a"));
+    EXPECT_TRUE(t_i_j->semantic_ == luna::SemanticOp_Write);
+    EXPECT_TRUE(t_i->semantic_ == luna::SemanticOp_Read);
+    EXPECT_TRUE(t->semantic_ == luna::SemanticOp_Read);
+    EXPECT_TRUE(i->semantic_ == luna::SemanticOp_Read);
+    EXPECT_TRUE(j->semantic_ == luna::SemanticOp_Read);
+    EXPECT_TRUE(a->semantic_ == luna::SemanticOp_Read);
+}
+
+TEST_CASE(semantic6)
+{
+    auto ast = Semantic("a = t.m.n");
+    auto t_m_n = ASTFind<luna::MemberAccessor>(ast, [](luna::MemberAccessor *ma) {
+        return ma->member_.str_->GetStdString() == "n";
+    });
+    auto t_m = ASTFind<luna::MemberAccessor>(ast, [](luna::MemberAccessor *ma) {
+        return ma->member_.str_->GetStdString() == "m";
+    });
+    auto t = ASTFind<luna::Terminator>(ast, NameFinder("t"));
+    auto a = ASTFind<luna::Terminator>(ast, NameFinder("a"));
+    EXPECT_TRUE(t_m_n->semantic_ == luna::SemanticOp_Read);
+    EXPECT_TRUE(t_m->semantic_ == luna::SemanticOp_Read);
+    EXPECT_TRUE(t->semantic_ == luna::SemanticOp_Read);
+    EXPECT_TRUE(a->semantic_ == luna::SemanticOp_Write);
+}
+
+TEST_CASE(semantic7)
+{
+    auto ast = Semantic("a = t[i][j]");
+    auto t_i_j = ASTFind<luna::IndexAccessor>(ast, [](luna::IndexAccessor *) {
+        return true;
+    });
+    auto t_i = ASTFind<luna::IndexAccessor>(t_i_j->table_, [](luna::IndexAccessor *) {
+        return true;
+    });
+    auto t = ASTFind<luna::Terminator>(ast, NameFinder("t"));
+    auto i = ASTFind<luna::Terminator>(ast, NameFinder("i"));
+    auto j = ASTFind<luna::Terminator>(ast, NameFinder("j"));
+    auto a = ASTFind<luna::Terminator>(ast, NameFinder("a"));
+    EXPECT_TRUE(t_i_j->semantic_ == luna::SemanticOp_Read);
+    EXPECT_TRUE(t_i->semantic_ == luna::SemanticOp_Read);
+    EXPECT_TRUE(t->semantic_ == luna::SemanticOp_Read);
+    EXPECT_TRUE(i->semantic_ == luna::SemanticOp_Read);
+    EXPECT_TRUE(j->semantic_ == luna::SemanticOp_Read);
+    EXPECT_TRUE(a->semantic_ == luna::SemanticOp_Write);
+}
+
+TEST_CASE(semantic8)
+{
+    auto ast = Semantic("t = { [i] = a, m = b, c }");
+    auto i = ASTFind<luna::Terminator>(ast, NameFinder("i"));
+    auto a = ASTFind<luna::Terminator>(ast, NameFinder("a"));
+    auto b = ASTFind<luna::Terminator>(ast, NameFinder("b"));
+    auto c = ASTFind<luna::Terminator>(ast, NameFinder("c"));
+    EXPECT_TRUE(i->semantic_ == luna::SemanticOp_Read);
+    EXPECT_TRUE(a->semantic_ == luna::SemanticOp_Read);
+    EXPECT_TRUE(b->semantic_ == luna::SemanticOp_Read);
+    EXPECT_TRUE(c->semantic_ == luna::SemanticOp_Read);
 }
