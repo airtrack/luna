e3022dd554afea284bf32393d545e53b65719a40
airtrack <superking.airtrack@gmail.com>
2014-02-02 23:03:11 +0800

    ‘ipairs’ library function.

--- src/BaseLib.cpp
+++ src/BaseLib.cpp
@@ -1,4 +1,5 @@
 #include "BaseLib.h"
+#include "Table.h"
 #include <stdio.h>
 
 namespace lib {
@@ -47,10 +48,72 @@ namespace base {
         return 0;
     }
 
+    int DoIPairs(luna::State *state)
+    {
+        luna::StackAPI api(state);
+        int params = api.GetStackSize();
+        if (params < 2)
+        {
+            api.ArgCountError(2);
+            return 0;
+        }
+
+        if (!api.IsTable(0))
+        {
+            api.ArgTypeError(0, luna::ValueT_Table);
+            return 0;
+        }
+
+        if (!api.IsNumber(1))
+        {
+            api.ArgTypeError(1, luna::ValueT_Number);
+            return 0;
+        }
+
+        luna::Table *t = api.GetTable(0);
+        double num = api.GetNumber(1) + 1;
+
+        luna::Value k;
+        k.type_ = luna::ValueT_Number;
+        k.num_ = num;
+        luna::Value v = t->GetValue(k);
+
+        if (v.type_ == luna::ValueT_Nil)
+            return 0;
+
+        api.PushValue(k);
+        api.PushValue(v);
+        return 2;
+    }
+
+    int IPairs(luna::State *state)
+    {
+        luna::StackAPI api(state);
+        int params = api.GetStackSize();
+        if (params < 1)
+        {
+            api.ArgCountError(1);
+            return 0;
+        }
+
+        if (!api.IsTable(0))
+        {
+            api.ArgTypeError(0, luna::ValueT_Table);
+            return 0;
+        }
+
+        luna::Table *t = api.GetTable(0);
+        api.PushCFunction(DoIPairs);
+        api.PushTable(t);
+        api.PushNumber(0);
+        return 3;
+    }
+
     void RegisterBaseLib(luna::State *state)
     {
         luna::Library lib(state);
         lib.RegisterFunc("print", Print);
+        lib.RegisterFunc("ipairs", IPairs);
     }
 
 } // namespace base
