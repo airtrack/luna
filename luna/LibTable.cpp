d65d20ccd54f3663d4349cb9a02c5d08213b0e3c
airtrack <superking.airtrack@gmail.com>
2014-09-03 20:21:46 +0800

    Issue #3: add table.concat.

--- luna/LibTable.cpp
+++ luna/LibTable.cpp
@@ -1,9 +1,84 @@
 #include "LibTable.h"
 #include "State.h"
 #include "Table.h"
+#include <sstream>
 
 namespace lib {
 namespace table {
+
+    // If the index value is number, then get the number value,
+    // else report type error.
+    template<typename NumType>
+    bool GetNumber(luna::StackAPI &api, int index, NumType &num)
+    {
+        if (!api.IsNumber(index))
+        {
+            api.ArgTypeError(index, luna::ValueT_Number);
+            return false;
+        }
+
+        num = static_cast<NumType>(api.GetNumber(index));
+        return true;
+    }
+
+    int Concat(luna::State *state)
+    {
+        luna::StackAPI api(state);
+        if (!api.CheckArgs(1, luna::ValueT_Table))
+            return 0;
+
+        auto table = api.GetTable(0);
+        const char *sep = "";
+        std::size_t i = 1;
+        std::size_t j = table->ArraySize();
+
+        auto params = api.GetStackSize();
+        if (params > 1)
+        {
+            // If the value of index 1 is string, then get the string as sep
+            if (api.IsString(1))
+            {
+                sep = api.GetString(1)->GetCStr();
+
+                // Try to get the range of table
+                if (params > 2 && !GetNumber(api, 2, i))
+                    return 0;
+
+                if (params > 3 && !GetNumber(api, 3, j))
+                    return 0;
+            }
+            else
+            {
+                // Try to get the range of table
+                if (!GetNumber(api, 1, i))
+                    return 0;
+
+                if (params > 2 && !GetNumber(api, 2, j))
+                    return 0;
+            }
+        }
+
+        luna::Value key;
+        key.type_ = luna::ValueT_Number;
+
+        // Concat values(number or string) of the range [i, j]
+        std::ostringstream oss;
+        for (; i <= j; ++i)
+        {
+            key.num_ = i;
+            auto value = table->GetValue(key);
+
+            if (value.type_ == luna::ValueT_Number)
+                oss << value.num_;
+            else if (value.type_ == luna::ValueT_String)
+                oss << value.str_->GetCStr();
+
+            if (i != j) oss << sep;
+        }
+
+        api.PushString(oss.str());
+        return 1;
+    }
 
     int Insert(luna::State *state)
     {
@@ -18,13 +93,9 @@ namespace table {
 
         if (params > 2)
         {
-            if (!api.IsNumber(1))
-            {
-                api.ArgTypeError(1, luna::ValueT_Number);
+            if (!GetNumber(api, 1, index))
                 return 0;
-            }
 
-            index = static_cast<decltype(index)>(api.GetNumber(1));
             value = 2;
         }
 
@@ -103,6 +174,7 @@ namespace table {
     {
         luna::Library lib(state);
         luna::TableMemberReg table[] = {
+            { "concat", Concat },
             { "insert", Insert },
             { "pack", Pack },
             { "remove", Remove },
